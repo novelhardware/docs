b1f5c44d1c702cd9ecdc91906765d9c7c3c6e758
Simon Sorensen <hello@simse.io>
2020-11-22 01:09:49 +0100

    initial commit

---
+++ docs/doc1.md
@@ -0,0 +1,12 @@
+---
+id: doc1
+title: Home
+sidebar_label: Home
+slug: /
+---
+
+Welcome to the Novel Hardware documentation.
+
+Here you can find detailed documentation on how to communicate with Novel Hardware creations through your own software. 
+
+You may also read up on how to write custom firmware for your products. They are, after all, **yours**!
---
+++ docusaurus.config.js
@@ -0,0 +1,98 @@
+module.exports = {
+  title: 'Novel Hardware',
+  tagline: 'Documentation for tinkering with my products ',
+  url: 'https://docs.novelhardware.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'novelhardware', // Usually your GitHub org/user name.
+  projectName: 'docs', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Novel Hardware',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Home',
+          position: 'left',
+        },
+        /*{
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },*/
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: 'blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          routeBasePath: '/',
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
